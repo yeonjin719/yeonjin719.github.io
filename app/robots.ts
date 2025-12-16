7f49921cb2da1c804628951152a65da8d589a7c6
Yeonjin Kim <kyj0719@gmail.com>
2025-12-16 18:43:42 +0900

    fix: 빌드 에러 해결

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,8 @@
 import type { MetadataRoute } from 'next';
 
+export const dynamic = 'force-static';
+export const revalidate = false;
+
 const siteUrl = 'https://yeonjin719.github.io';
 
 export default function robots(): MetadataRoute.Robots {
